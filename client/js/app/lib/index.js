614f9c2605e6f121eba8b3e76c40d9adc2244416
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-04-30 17:47:56 -0300

    Construindo estrutura de pastas e instanciando a classe Negociacao

---
+++ client/js/app/lib/index.js
@@ -0,0 +1,27 @@
+var campos = [
+    document.querySelector("#data"),
+    document.querySelector("#quantidade"),
+    document.querySelector("#valor"),
+]
+
+var formulario = document.querySelector(".form");
+var tbody = document.querySelector("table tbody");
+
+formulario.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    var tr = document.createElement("tr");
+
+    campos.forEach(function(campo){
+
+        var td = document.createElement("td");
+        td.textContent = campo.value;
+        tr.appendChild(td);
+    });
+
+    var tdVolume = document.createElement("td");
+    tdVolume.textContent = (campos[1].value * campos[2].value);
+    tr.appendChild(tdVolume);
+
+    tbody.appendChild(tr);
+});
